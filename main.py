4530bd5ffead4ecd673ea16ebcab56a23ff38e37
MOVS Laboratory <lab.movs@outlook.com>
2019-11-16 18:41:51 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import sys
+from random import randint
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QBrush, QPen
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+from PyQt5.QtCore import Qt
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.f = False
+        self.round = []
+        self.pushButton.clicked.connect(self.change)
+
+    def paintEvent(self, event):
+        if self.f:
+            self.round.append((randint(10, 990), randint(50, 350), randint(10, 50)))
+            painter = QPainter()
+            painter.begin(self)
+            painter.setPen(QPen(Qt.yellow, 8, Qt.SolidLine))
+            painter.setBrush(QBrush(Qt.yellow, Qt.SolidPattern))
+            for i in range(len(self.round)):
+                painter.drawEllipse(self.round[i][0], self.round[i][1], self.round[i][2], self.round[i][2])
+            painter.end()
+            self.f = False
+
+    def change(self):
+        self.f = True
+        self.update()
+
+
+app = QApplication(sys.argv)
+ex = MyWidget()
+ex.show()
+sys.exit(app.exec_())
